d5c68df47bc8c919c7923ae3b37e81866aa217b4
SHUBHAM SETH <shubham.seth@greenpaynetwork.co.in>
2026-06-21 12:45:00 +0530

    Practice: Sending DELETE Requests

--- src/app/places/user-places/user-places.component.ts
+++ src/app/places/user-places/user-places.component.ts
@@ -21,21 +21,29 @@ export class UserPlacesComponent implements OnInit {
   private placesService = inject(PlacesService);
   private destroyRef = inject(DestroyRef);
   places = this.placesService.loadedUserPlaces;
-    
+
   ngOnInit() {
-      this.isFetching.set(true);
-      const subscription = this.placesService.loadUserPlaces()
-        .subscribe({      
-          error: (error: Error) => {
-            this.error.set(error.message);
-          },
-          complete: () => {
-            this.isFetching.set(false);
-          }
+    this.isFetching.set(true);
+    const subscription = this.placesService.loadUserPlaces()
+      .subscribe({
+        error: (error: Error) => {
+          this.error.set(error.message);
+        },
+        complete: () => {
+          this.isFetching.set(false);
+        }
       });
-  
-      this.destroyRef.onDestroy(() => {
-        subscription.unsubscribe();
-      })
-    }
+
+    this.destroyRef.onDestroy(() => {
+      subscription.unsubscribe();
+    })
+  }
+
+  onRemovePlace(place: Place) {
+    const subscription = this.placesService.removeUserPlace(place).subscribe();
+
+    this.destroyRef.onDestroy(() => {
+      subscription.unsubscribe();
+    })
+  }
 }
