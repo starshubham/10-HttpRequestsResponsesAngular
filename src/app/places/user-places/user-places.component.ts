21092a4a0e1a0ef13f6c78cab56494453518a946
SHUBHAM SETH <shubham.seth@greenpaynetwork.co.in>
2026-06-21 08:39:23 +0530

    Managing HTTP-loaded Data via s Service

--- src/app/places/user-places/user-places.component.ts
+++ src/app/places/user-places/user-places.component.ts
@@ -1,4 +1,4 @@
-import { Component, DestroyRef, inject, signal } from '@angular/core';
+import { Component, DestroyRef, inject, OnInit, signal } from '@angular/core';
 
 import { PlacesContainerComponent } from '../places-container/places-container.component';
 import { PlacesComponent } from '../places.component';
@@ -14,20 +14,18 @@ import { PlacesService } from '../places.service';
   styleUrl: './user-places.component.css',
   imports: [PlacesContainerComponent, PlacesComponent],
 })
-export class UserPlacesComponent {
-  places = signal<Place[] | undefined>(undefined);
+export class UserPlacesComponent implements OnInit {
+  // places = signal<Place[] | undefined>(undefined);
   isFetching = signal(false);
   error = signal('');
   private placesService = inject(PlacesService);
   private destroyRef = inject(DestroyRef);
+  places = this.placesService.loadedUserPlaces;
     
   ngOnInit() {
       this.isFetching.set(true);
       const subscription = this.placesService.loadUserPlaces()
-        .subscribe({
-          next: (places) => {
-            this.places.set(places);
-          },
+        .subscribe({      
           error: (error: Error) => {
             this.error.set(error.message);
           },
